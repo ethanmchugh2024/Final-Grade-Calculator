95491181878a2b08f5127d0b4a428dffea63cef6
Ethan McHugh <ethanmchugh2024@gmail.com>
2022-07-21 14:53:07 -0500

    Initial Commit

---
+++ Final Grade Calculator/Final_Grade_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Grade_CalculatorApp.swift
+//  Final Grade Calculator
+//
+//  Created by Ethan McHugh on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Final_Grade_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
